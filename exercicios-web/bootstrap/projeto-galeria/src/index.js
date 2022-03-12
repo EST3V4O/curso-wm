85bc1ef9b952aebc1ea2b002eddb41611ceed5f1
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-03-12 11:06:00 -0300

    aula 394

---
+++ exercicios-web/bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivo SASS
+import './scss/index.scss'
+
+// Dependencies
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos JS
+import './js/cors/includes'
+import './js/plugins/cityButtons'
