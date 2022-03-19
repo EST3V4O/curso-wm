e99b0feec1085edc234ceae841d1f7cf34785578
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-03-19 13:23:48 -0300

    aula 439

---
+++ exercicios-web/react/nextjs/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+  // props é somente leitura
+  return (
+    <>
+      <Cabecalho titulo="Next.js & React" />
+      <Cabecalho titulo="Apreenda Next na prática " />
+    </>
+  )
+}
