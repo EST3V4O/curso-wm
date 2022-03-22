fdde017185fd1f73b81a488102471eaa534b7fce
Estevão <teh.gabriel10@gmail.com>
2022-03-21 21:25:16 -0300

    aula 489

---
+++ exercicios-web/vue/calculator/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+
+export default {
+  components: { Calculator }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'RobotoMono';
+  src: url('./fonts/RobotoMono-Thin.ttf');
+}
+
+* {
+  font-family: 'RobotoMono', monos;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgba(83, 108, 118), rgba(41, 46, 73));
+}
+</style>
