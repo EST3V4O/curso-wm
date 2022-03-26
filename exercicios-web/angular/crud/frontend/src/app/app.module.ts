b7bead45c274ad8643119d299f4870b704dea3a1
Estevão <teh.gabriel10@gmail.com>
2022-03-26 16:16:03 -0300

    aula 550

--- exercicios-web/angular/crud/frontend/src/app/app.module.ts
+++ exercicios-web/angular/crud/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,11 +9,37 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
 
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatCardModule } from '@angular/material/card'
 import { MatListModule } from '@angular/material/list';
-import { HomeComponent } from './views/home/home.component'
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snackbar';
+
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+
+import { FormsModel } from "@angular/forms"
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormInputModule } from '@angular/material/form-input';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
+
+import localePt from '@angular/commom/locales/pt';
+import { registerLocaleData } from '@angular/commom';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -21,18 +47,38 @@ import { HomeComponent } from './views/home/home.component'
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    MatSidenavModule,
-    MatListModule,
     HomeComponent,
-    MatCardModule
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
+    ProductCrudComponent,
+    ProductReadComponent,
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    FormsModel,
+    MatFormFieldModule,
+    MatFormInputModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ exercicios-web/angular/crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product = {} as Product
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') as string
+    this.productService.readById(id).subscribe(product => {
+      this.product = this.product
+    })
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.product.id as number).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso!')
+      this.router.navigate(["/products"])
+    })
+  }
+  
+  cancel(): void {
+    this.router.navigate(["/products"])
+  }
+}
