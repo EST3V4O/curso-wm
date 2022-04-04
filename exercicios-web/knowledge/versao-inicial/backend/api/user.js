13be535774fa580959aadf3007b9f96ebc7154fb
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-04-04 20:55:24 -0300

    aula 634

--- exercicios-web/knowledge/versao-inicial/backend/api/user.js
+++ exercicios-web/knowledge/versao-inicial/backend/api/user.js
@@ -1,17 +1,19 @@
-const bcryptjs = require('bcrypt-nodejs')
+const bcrypt = require('bcrypt-nodejs')
 
 module.exports = app => {
   const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
   const encryptPassword = password => {
-    const salt = bcryptjs.genSaltSync(10)
-
-    return bcryptjs.hashSync(password, salt)
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
   }
 
   const save = async (req, res) => {
     const user = { ...req.body }
     if(req.params.id) user.id = req.params.id
+
+    if(!req.originalUrl.startsWith('/users')) user.admin = false
+    if(!req.user || !req.user.admin) user.admin = false
 
     try {
       existsOrError(user.name, 'Nome não informado')
@@ -22,22 +24,22 @@ module.exports = app => {
           'Senhas não conferem')
 
       const userFromDB = await app.db('users')
-        .where({ email: suer.email }).first()
-      if(user.id) {
+        .where({ email: user.email }).first()
+      if(!user.id) {
         notExistsOrError(userFromDB, 'Usuário já cadastrado')
       }
-
-    } catch (msg) {
+    } catch(msg) {
       return res.status(400).send(msg)
     }
 
-    user.password = encryptPassword(user.password.password)
+    user.password = encryptPassword(user.password)
     delete user.confirmPassword
 
     if(user.id) {
       app.db('users')
         .update(user)
         .where({ id: user.id })
+        .whereNull('deletedAt')
         .then(_ => res.status(204).send())
         .catch(err => res.status(500).send(err))
     } else {
@@ -51,6 +53,7 @@ module.exports = app => {
   const get = (req, res) => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
+      .whereNull('deletedAt')
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
@@ -59,10 +62,28 @@ module.exports = app => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
       .where({ id: req.params.id })
+      .whereNull('deletedAt')
       .first()
       .then(user => res.json(user))
       .catch(err => res.status(500).send(err))
   }
 
-  return { save, get, getById }
+  const remove = async (req, res) => {
+    try {
+      const articles = await app.db('articles')
+          .where({ userId: req.params.id })
+      notExistsOrError(articles, 'Usuário possui artigos.')
+
+      const rowsUpdated = await app.db('users')
+          .update({deletedAt: new Date()})
+          .where({ id: req.params.id })
+      existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
+
+      res.status(204).send()
+    } catch(msg) {
+      res.status(400).send(msg)
+    }
+  }
+
+  return { save, get, getById, remove }
 }
